b36e8ce499a127b6e2bd89e43c6ecccde4b3b2b5
aavaig-malhotra <aavaigmalhotra2001@gmail.com>
2022-09-13 20:19:56 +0530

    add : new text

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
-import logo from './logo.svg';
-import './App.css';
+import logo from "./logo.svg";
+import "./App.css";
 
 function App() {
   return (
@@ -7,7 +7,8 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+          Edit <code>src/App.js</code> and save to reload. This is my new test
+          case
         </p>
         <a
           className="App-link"
